ab90a0aabcb67187dd3939ee54988e122d1a9b05
Sadat-09 <85846183+Sadat-09@users.noreply.github.com>
2024-08-13 21:37:54 +0600

    code

---
+++ A_Only_Pluses.cpp
@@ -0,0 +1,63 @@
+
+#include <bits/stdc++.h>
+#define IO                            \
+    ios_base::sync_with_stdio(false); \
+    cin.tie(NULL);                    \
+    cout.tie(NULL)
+typedef long long int ll;
+typedef long double LD;
+#define w(t)     \
+    int t;       \
+    cin >> t;    \
+    while (t--)  \
+    {            \
+        solve(); \
+    }
+using namespace std;
+bool check_prime(int n) {
+  bool is_prime = true;
+
+  // 0 and 1 are not prime numbers
+  if (n == 0 || n == 1) {
+    is_prime = false;
+  }
+
+  for (int i = 2; i <= n / 2; ++i) {
+    if (n % i == 0) {
+      is_prime = false;
+      break;
+    }
+  }
+
+  return is_prime;
+}
+
+
+void solve()
+{
+     int a, b, c;
+        cin >> a >> b >> c;
+
+        int maxi = a * b * c;
+
+        
+        for (int i = 0; i <= 5; ++i) {
+            for (int j = 0; j <= 5 - i; ++j) {
+                int k = 5 - i - j;
+                int a2 = a + i;
+                int b2 = b + j;
+                int c2 = c + k;
+                maxi = max(maxi, a2 * b2 * c2);
+            }
+        }
+
+        cout << maxi << endl;
+}
+
+int main()
+{
+    IO;
+    //solve();
+    w(t);
+    return 0;
+}
---
+++ B_Angry_Monk.cpp
@@ -0,0 +1,66 @@
+
+#include <bits/stdc++.h>
+#define IO                            \
+    ios_base::sync_with_stdio(false); \
+    cin.tie(NULL);                    \
+    cout.tie(NULL)
+typedef long long int ll;
+typedef long double LD;
+#define w(t)     \
+    int t;       \
+    cin >> t;    \
+    while (t--)  \
+    {            \
+        solve(); \
+    }
+using namespace std;
+bool check_prime(int n) {
+  bool is_prime = true;
+
+  // 0 and 1 are not prime numbers
+  if (n == 0 || n == 1) {
+    is_prime = false;
+  }
+
+  for (int i = 2; i <= n / 2; ++i) {
+    if (n % i == 0) {
+      is_prime = false;
+      break;
+    }
+  }
+
+  return is_prime;
+}
+
+
+void solve()
+{
+    int n,k;
+    cin>>n>>k;
+    int arr[k];
+    for (int i = 0; i < k; i++)
+    {
+        /* code */
+        cin>>arr[i];
+    }
+    sort(arr,arr+k);
+    
+    int a= abs(arr[k-1]-n);
+
+    int sum=0;
+    for (int i = 0; i < k-1; i++)
+    {
+        sum+=arr[i];
+    }
+    cout<<a+(sum/2)<<endl;
+    
+    
+}
+
+int main()
+{
+    IO;
+    //solve();
+    w(t);
+    return 0;
+}
